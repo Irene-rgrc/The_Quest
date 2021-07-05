47b5a934c08aa1d3e2dc5ea99cc1d285e7a16752
Irene-rgrc <82141991+Irene-rgrc@users.noreply.github.com>
2021-07-05 19:57:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from TheQuest.game import Thequest
+import pygame as pg
+from TheQuest.escenes import Game
+from TheQuest import ANCHO, ALTURA
+
+
+
+if __name__ == '__main__':
+    pg.init()
+    controler = Thequest()
+    controler.start()
+    
+    
